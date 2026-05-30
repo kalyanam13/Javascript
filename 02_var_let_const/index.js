cc10d4dacd5cb8f01b43536f019c06b3acf99dc3
kalyanam13 <kalyanamchakrawarty66@gmail.com>
2026-05-30 10:01:35 +0530

    Added var_let_const

---
+++ 02_var_let_const/index.js
@@ -0,0 +1,17 @@
+console.log("This is the tutorial of var, let and const in JavaScript")
+
+var a =  67
+var a = "siya"
+var b = "kalyanam"
+var c = true
+var d = null
+var e = undefined
+
+ 
+{
+    var b = "Chakrawarty"
+    console.log(b)
+}
+console.log(b)
+
+
---
+++ 02_var_let_const/index_.js
@@ -0,0 +1,20 @@
+console.log("This is the tutorial of var, let and const in JavaScript")
+
+let a =  67
+let a = "siya"
+var b = "kalyanam"
+var c = true
+var d = null
+var e = undefined
+
+const f = "This is a constant variable"
+
+console.log(a)
+console.log(f)
+
+ 
+{
+    let b = "Chakrawarty"
+    console.log(b)
+}
+console.log(b)
