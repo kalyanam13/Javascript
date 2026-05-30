6966c88cb06a8a421611676823be13c0216a4cc6
kalyanam13 <kalyanamchakrawarty66@gmail.com>
2026-05-30 09:30:41 +0530

    Added Variables concept

---
+++ 01_Variables_In_JS/index.js
@@ -0,0 +1,10 @@
+let a = 67
+console.log(a)
+a = "kalyanam"
+console.log(a) 
+
+// let 8sammy = 7 // Not allowed this will give error because variable name cannot start with number
+
+// let var = 7 // Not allowed this will give error because var is a reserved keyword in JavaScript
+
+
