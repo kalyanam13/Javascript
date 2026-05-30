885502f099e35b9a2585edc97299f24c940d6d5c
kalyanam13 <kalyanamchakrawarty66@gmail.com>
2026-05-30 10:54:38 +0530

    Added Primitive and Object

---
+++ 03_Primitive_and_Object/index.js
@@ -0,0 +1,21 @@
+// nn bb ss u
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("567");
+let e = "Hello World";
+let f = Symbol("I am a nice symbol");
+let g = undefined; // let g; // also undefined
+
+console.log(a, b, c, d, e, f, g);
+console.log(typeof d);
+
+// Objects
+const item = {
+    "name": true,
+    "price": 30000,
+    "brand": "HP",
+    "color": "silver"
+}
+
+console.log(item["name"]);
