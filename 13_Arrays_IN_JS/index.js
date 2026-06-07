9c83193f2fa4cdcf0c9eeb72836b6701a7f1d034
kalyanam13 <kalyanamchakrawarty66@gmail.com>
2026-06-07 08:58:26 +0530

    Added arrays

---
+++ 13_Arrays_IN_JS/index.js
@@ -0,0 +1,8 @@
+let marks_class_12 = [90, 85, 78, 92, 88 , null , false , "N/A"]; // Array of marks for class 12 students
+console.log(marks_class_12); // Output: [90, 85, 78, 92, 88]
+
+console.log(marks_class_12[0]); // Output: 90 (first element)
+console.log(marks_class_12[2]);
+console.log(marks_class_12[5]); // Output: null (6th element)
+console.log(marks_class_12.length);
+
